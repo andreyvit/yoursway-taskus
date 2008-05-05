87d85687ce38135f465f64d255ff4dd7dec04578
Mikhail Kalugin <mikhail.kalugin@gmail.com>
2008-05-05 21:09:12 +0700

    Added modern background color to the outline view.

--- com.mkalugin.corchy.ui/src/com/mkalugin/corchy/internal/ui/OutlineView.java
+++ com.mkalugin.corchy.ui/src/com/mkalugin/corchy/internal/ui/OutlineView.java
@@ -5,6 +5,8 @@ import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.ListViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.internal.cocoa.NSColor;
+import org.eclipse.swt.internal.cocoa.NSTableView;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 
@@ -20,10 +22,12 @@ public class OutlineView {
 		viewer = new ListViewer(parent, SWT.V_SCROLL);
 		Control control = viewer.getControl();
 		control.setLayoutData(GridDataFactory.fillDefaults().align(SWT.FILL,
-				SWT.FILL).grab(true, true).create());		
+				SWT.FILL).grab(true, true).create());
+		NSTableView tableView = (NSTableView) control.view;
+		tableView.setBackgroundColor(NSColor.colorWithDeviceRed((float)209.0/255, (float)215.0/255, (float)226.0/255, 1));
 		viewer.setContentProvider(new ArrayContentProvider());
 		viewer.setLabelProvider(new LabelProvider());
-		viewer.setInput(new String[] { "Pizda", "Hui" });
+		viewer.setInput(new String[] { "Pizda", "Hui" });		
 	}
 
 }
