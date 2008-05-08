0e96f2e12ac5acf837d1c6dd0d67c83b618304c3
Mikhail Kalugin <mikhail.kalugin@gmail.com>
2008-05-08 16:21:54 +0700

    FSStorage test fix (overridden storageIsEmpty()).

--- com.mkalugin.pikachu.core.tests/src/com/mkalugin/pikachu/core/tests/FileStorageTests.java
+++ com.mkalugin.pikachu.core.tests/src/com/mkalugin/pikachu/core/tests/FileStorageTests.java
@@ -1,5 +1,7 @@
 package com.mkalugin.pikachu.core.tests;
 
+import org.junit.Test;
+
 import com.mkalugin.pikachu.core.storage.DataStorage;
 import com.mkalugin.pikachu.core.storage.file.FSDataStorage;
 import com.mkalugin.pikachu.core.util.FileUtils;
@@ -9,6 +11,12 @@ public class FileStorageTests extends StorageConfomanceTests {
 	@Override
 	protected DataStorage createStorage() throws Exception {
 		return new FSDataStorage(FileUtils.createTempFolder("filestorage", "test"));
+	}
+	
+	@Test
+	@Override
+	public void storageIsEmpty() throws Exception {
+		assertEquals(1, storage.log().length);
 	}
 	
 }
