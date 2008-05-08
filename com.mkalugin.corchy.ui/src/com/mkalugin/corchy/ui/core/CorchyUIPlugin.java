5e791b72266c33cd8475ad889d5f41b18e7e0a1e
Mikhail Kalugin <mikhail.kalugin@gmail.com>
2008-05-08 22:41:46 +0700

    Moved dialog settings to ~/Library/Preferences/Corchy.app/dialog_settings.xml.

--- com.mkalugin.corchy.ui/src/com/mkalugin/corchy/ui/core/CorchyUIPlugin.java
+++ com.mkalugin.corchy.ui/src/com/mkalugin/corchy/ui/core/CorchyUIPlugin.java
@@ -1,17 +1,78 @@
 package com.mkalugin.corchy.ui.core;
-import org.eclipse.ui.plugin.AbstractUIPlugin;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.eclipse.jface.dialogs.DialogSettings;
+import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 public class CorchyUIPlugin extends AbstractUIPlugin {
 
-	private static CorchyUIPlugin instance; 
-	
+	private static final String DIALOG_SETTINGS_FILE = "dialog_settings.xml"; //$NON-NLS-1$
+
+	private static CorchyUIPlugin instance;
+
+	private DialogSettings dialogSettings = null;
+
 	public CorchyUIPlugin() {
-		instance = this;	
+		instance = this;
 	}
 
 	public static CorchyUIPlugin instance() {
 		return instance;
+	}
+
+	public IDialogSettings getDialogSettings() {
+		if (dialogSettings == null) {
+			loadDialogSettings();
+		}
+		return dialogSettings;
+	}
+
+	protected File preferencesDir() {
+		return new File(System.getProperty("user.home"), "Library/Preferences/Corchy.app/");
+	}
+
+	protected void loadDialogSettings() {
+		dialogSettings = new DialogSettings("CorchyUIPlugin"); //$NON-NLS-1$
+
+		FileReader in = null;
+		try {
+			in = new FileReader(new File(preferencesDir(), DIALOG_SETTINGS_FILE));
+			BufferedReader reader = new BufferedReader(in);
+			dialogSettings.load(reader);
+		} catch (IOException e) {
+			// load failed so ensure we have an empty settings
+			dialogSettings = new DialogSettings("CorchyUIPlugin"); //$NON-NLS-1$
+		} finally {
+			try {
+				if (in != null)
+					in.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	protected void saveDialogSettings() {
+		if (dialogSettings == null) {
+			return;
+		}
+
+		try {
+			File prefs = preferencesDir();
+			if (!prefs.exists())
+				prefs.mkdirs();
+			dialogSettings.save(new File(prefs, DIALOG_SETTINGS_FILE).getAbsolutePath());
+		} catch (IOException e) {
+			// spec'ed to ignore problems
+		} catch (IllegalStateException e) {
+			// spec'ed to ignore problems
+		}
 	}
 
 }
