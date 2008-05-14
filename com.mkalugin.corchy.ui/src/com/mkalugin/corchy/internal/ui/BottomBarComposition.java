cd049e734940dd74c44ae881a4a25b1f50817e5e
Mikhail Kalugin <mikhail.kalugin@gmail.com>
2008-05-15 00:12:46 +0700

    Made the bottom bar native (removed the idiotic hand-made gradient drawing).

--- com.mkalugin.corchy.ui/src/com/mkalugin/corchy/internal/ui/BottomBarComposition.java
+++ com.mkalugin.corchy.ui/src/com/mkalugin/corchy/internal/ui/BottomBarComposition.java
@@ -3,18 +3,24 @@ package com.mkalugin.corchy.internal.ui;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.internal.cocoa.NSWindow;
+import org.eclipse.swt.internal.cocoa.OS;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
 
 public class BottomBarComposition {
     
     private Composite body;
-    private BottomBar bottomBar;
+    private Composite bottomBar;
     
-    public BottomBarComposition(Composite parent) {
+    public BottomBarComposition(Shell parent) {
+    	NSWindow window = parent.view.window();
+    	window.setContentBorderThickness(32, OS.NSMinYEdge);
+    	
         body = new Composite(parent, SWT.NONE);
         body.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
         
-        bottomBar = new BottomBar(parent);
+        bottomBar = new Composite(parent, SWT.NONE);
         bottomBar.setLayoutData(GridDataFactory.fillDefaults().grab(true, false).minSize(SWT.DEFAULT, 32)
                 .hint(SWT.DEFAULT, 32).create());
         GridLayoutFactory.fillDefaults().extendedMargins(0, 0, 0, 0).spacing(0, 0).numColumns(1)
@@ -25,7 +31,7 @@ public class BottomBarComposition {
         return body;
     }
     
-    public BottomBar bottomBar() {
+    public Composite bottomBar() {
         return bottomBar;
     }
     
