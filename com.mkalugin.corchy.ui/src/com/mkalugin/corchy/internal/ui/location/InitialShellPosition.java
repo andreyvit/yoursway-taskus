654ddb5c70b80e6c15b8a016377a99cb90e9a48d
Andrey Tarantsov <andreyvit@gmail.com>
2008-05-14 17:50:53 +0700

    Implemented real centering.

--- com.mkalugin.corchy.ui/src/com/mkalugin/corchy/internal/ui/location/InitialShellPosition.java
+++ com.mkalugin.corchy.ui/src/com/mkalugin/corchy/internal/ui/location/InitialShellPosition.java
@@ -21,30 +21,43 @@ public enum InitialShellPosition {
     },
     
     CENTERED {
+        
+        @Override
+        public Point calculatePosition(Display display, Shell parent, Point initialSize) {
+            return calculateCenteredPosition(display, parent, initialSize, 1, 2);
+        }
+        
+    },
+    
+    SHIFTED_CENTERED {
 
         @Override
         public Point calculatePosition(Display display, Shell parent, Point initialSize) {
-            Monitor monitor = display.getPrimaryMonitor();
-            if (parent != null) {
-                monitor = parent.getMonitor();
-            }
-
-            Rectangle monitorBounds = monitor.getClientArea();
-            Point centerPoint;
-            if (parent != null) {
-                centerPoint = Geometry.centerPoint(parent.getBounds());
-            } else {
-                centerPoint = Geometry.centerPoint(monitorBounds);
-            }
-
-            return new Point(centerPoint.x - (initialSize.x / 2), Math.max(
-                    monitorBounds.y, Math.min(centerPoint.y
-                            - (initialSize.y * 2 / 3), monitorBounds.y
-                            + monitorBounds.height - initialSize.y)));
+            return calculateCenteredPosition(display, parent, initialSize, 2, 3);
         }
-    
+   
     };
     
     public abstract Point calculatePosition(Display display, Shell parent, Point initialSize);
+
+    static Point calculateCenteredPosition(Display display, Shell parent, Point initialSize, int factorA, int factorB) {
+        Monitor monitor = display.getPrimaryMonitor();
+        if (parent != null) {
+            monitor = parent.getMonitor();
+        }
+
+        Rectangle monitorBounds = monitor.getClientArea();
+        Point centerPoint;
+        if (parent != null) {
+            centerPoint = Geometry.centerPoint(parent.getBounds());
+        } else {
+            centerPoint = Geometry.centerPoint(monitorBounds);
+        }
+
+        return new Point(centerPoint.x - (initialSize.x / 2), Math.max(
+                monitorBounds.y, Math.min(centerPoint.y
+                        - (initialSize.y * factorA / factorB), monitorBounds.y
+                        + monitorBounds.height - initialSize.y)));
+    }
     
 }
