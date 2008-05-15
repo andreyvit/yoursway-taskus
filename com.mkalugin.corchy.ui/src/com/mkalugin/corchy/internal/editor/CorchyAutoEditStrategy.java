c1c4063617b831ed7c3897da65171336ef40dc83
Andrey Tarantsov <andreyvit@gmail.com>
2008-05-16 03:31:01 +0700

    Added an auto-edit strategy to automatically add "- " when inserting a new line.

---
+++ com.mkalugin.corchy.ui/src/com/mkalugin/corchy/internal/editor/CorchyAutoEditStrategy.java
@@ -0,0 +1,30 @@
+package com.mkalugin.corchy.internal.editor;
+
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.DocumentCommand;
+import org.eclipse.jface.text.IAutoEditStrategy;
+import org.eclipse.jface.text.IDocument;
+
+public class CorchyAutoEditStrategy implements IAutoEditStrategy {
+
+    public void customizeDocumentCommand(IDocument document, DocumentCommand command) {
+        if (command.length == 0 && command.text.equals("\n"))
+            customizeNewLineInsertion(document, command);
+    }
+
+    private void customizeNewLineInsertion(IDocument document, DocumentCommand command) {
+        try {
+            int line = document.getLineOfOffset(command.offset);
+            int lineStart = document.getLineOffset(line);
+            int lineLength = document.getLineLength(line);
+            String lineText = document.get(lineStart, lineLength);
+            if (lineText.startsWith("- ") || lineText.trim().endsWith(":")) {
+                command.text = "\n- ";
+            }
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+}
--- com.mkalugin.corchy.ui/src/com/mkalugin/corchy/internal/editor/CorchySourceViewerConfiguration.java
+++ com.mkalugin.corchy.ui/src/com/mkalugin/corchy/internal/editor/CorchySourceViewerConfiguration.java
@@ -1,5 +1,6 @@
 package com.mkalugin.corchy.internal.editor;
 
+import org.eclipse.jface.text.IAutoEditStrategy;
 import org.eclipse.jface.text.presentation.IPresentationReconciler;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.SourceViewerConfiguration;
@@ -9,6 +10,11 @@ public class CorchySourceViewerConfiguration extends SourceViewerConfiguration {
     @Override
     public IPresentationReconciler getPresentationReconciler(ISourceViewer sourceViewer) {
         return null;
+    }
+    
+    @Override
+    public IAutoEditStrategy[] getAutoEditStrategies(ISourceViewer sourceViewer, String contentType) {
+        return new IAutoEditStrategy[] {new CorchyAutoEditStrategy()};
     }
     
 }
