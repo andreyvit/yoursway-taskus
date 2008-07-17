c552abad996db887f1bbb8c633cdf0123c016709
Mikhail Kalugin <mikhail.kalugin@gmail.com>
2008-07-18 03:44:00 +0700

    Simplest completion.

--- com.mkalugin.corchy.ui/src/com/mkalugin/corchy/internal/ui/editor/CorchyCompletionProcessor.java
+++ com.mkalugin.corchy.ui/src/com/mkalugin/corchy/internal/ui/editor/CorchyCompletionProcessor.java
@@ -1,5 +1,12 @@
 package com.mkalugin.corchy.internal.ui.editor;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.contentassist.CompletionProposal;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
@@ -10,10 +17,32 @@ import org.eclipse.jface.text.contentassist.IContextInformationValidator;
 public class CorchyCompletionProcessor implements IContentAssistProcessor {
 
 	public ICompletionProposal[] computeCompletionProposals(ITextViewer viewer, int offset) {
-		return new ICompletionProposal[] { 
-				new CompletionProposal("bar", offset, 3, offset), 
-				new CompletionProposal("foo", offset, 3, offset) 
-		};
+		String string = viewer.getDocument().get();
+		List<ICompletionProposal> proposals = new ArrayList<ICompletionProposal>();
+		Set<String> tags = new HashSet<String>();
+
+		String prefix = string.substring(Math.max(0, offset - 10), offset);
+		int atin = prefix.indexOf("@");
+		if (atin == -1)
+			return new ICompletionProposal[0];
+
+		prefix = prefix.substring(atin);
+
+		Pattern pattern = Pattern.compile("@\\w+");
+		Matcher matcher = pattern.matcher(string);
+		while (matcher.find()) {
+			String group = matcher.group();
+			if (group.startsWith(prefix) && group.length() > prefix.length())
+				tags.add(group);
+		}
+
+		for (String tag : tags) {
+			proposals.add(new CompletionProposal(tag.substring(prefix.length()), offset, 0, tag
+					.length()
+					- prefix.length()));
+		}
+
+		return proposals.toArray(new ICompletionProposal[proposals.size()]);
 	}
 
 	public IContextInformation[] computeContextInformation(ITextViewer viewer, int offset) {
