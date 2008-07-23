2dfc6c795366cc11fa3cb1bdf523ce28fbcdedd6
Alexander Sheboltaev <a.sheboltaev@gmail.com>
2008-07-23 19:38:13 +0700

    CoolScrollBarStyledTextBinding for other composites.

---
+++ com.mkalugin.swthell/src/com/mkalugin/swthell/CoolScrollBarStyledTextBinding.java
@@ -0,0 +1,96 @@
+package com.mkalugin.swthell;
+
+import static com.mkalugin.swthell.CompositeUtils.addAllChildrenListener;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseWheelListener;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+
+public class CoolScrollBarStyledTextBinding {
+    
+    private final StyledText styledText;
+    private final CoolScrollBar vScrollBar;
+    private final Composite composite;
+    
+    private int widgetHeight;
+    private int textHeight;
+    private int topPixel;
+    
+    public CoolScrollBarStyledTextBinding(StyledText styledText, CoolScrollBar scrollBar, Composite composite) {
+        this.styledText = styledText;
+        this.vScrollBar = scrollBar;
+        this.composite = composite;
+        
+        installListener();
+    }
+    
+    private void installListener() {
+        styledText.addMouseWheelListener(new MouseWheelListener() {
+            
+            public void mouseScrolled(MouseEvent e) {
+                vScroll(e.count, true);
+            }
+            
+        });
+        Listener updateListener = new Listener() {
+            
+            public void handleEvent(Event event) {
+                widgetHeight = styledText.getBounds().height;
+                int charCount = styledText.getCharCount();
+                if (charCount > 0)
+                    textHeight = styledText.getTextBounds(0, charCount - 1).height;
+                else
+                    textHeight = 0;
+                topPixel = styledText.getTopPixel();
+                vScrollBar.setRunnerSize(textHeight, widgetHeight);
+                vScrollBar.setPosition(topPixel);
+            }
+            
+        };
+        styledText.addListener(SWT.Modify, updateListener);
+        styledText.addListener(SWT.Traverse, updateListener);
+        styledText.addListener(SWT.Resize, updateListener);
+        new MouseEnterExitTracker(composite, new Listener() {
+            
+            public void handleEvent(Event event) {
+                if (event.type == SWT.MouseEnter) {
+                    vScrollBar.animateShow();
+                } else if (event.type == SWT.MouseExit) {
+                    vScrollBar.animateHide();
+                }
+                
+            }
+            
+        });
+        addAllChildrenListener(composite, SWT.MouseMove, new Listener() {
+            
+            public void handleEvent(Event event) {
+                vScrollBar.animateShow();
+            }
+            
+        });
+        vScrollBar.addListener(SWT.Selection, new Listener() {
+            
+            public void handleEvent(Event event) {
+                topPixel = (int) vScrollBar.getPosition();
+                styledText.setTopPixel(topPixel);
+            }
+            
+        });
+    }
+    
+    public void vScroll(int count, boolean notify) {
+        topPixel -= 5 * count;
+        if (topPixel < 0)
+            topPixel = 0;
+        if (topPixel > textHeight - widgetHeight)
+            topPixel = textHeight - widgetHeight;
+        styledText.setTopPixel(topPixel);
+        vScrollBar.setPosition(topPixel);
+    }
+    
+}
